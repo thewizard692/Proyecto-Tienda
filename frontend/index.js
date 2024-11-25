71d782f58bd571a997f87de307dfcf56e3f784bf
Marcos M Gonzalez <marcosmglezglez@hotmail.com>
2024-11-25 10:21:32 -0600

    ajustes al index y pagina de productos

--- frontend/index.js
+++ frontend/index.js
@@ -4,8 +4,10 @@ const alertContainer = document.getElementById('alertContainer')
 const productTableBody = document.getElementById('productTableBody')
 const btnSubmit = document.getElementById('submitBtn')
 
+//Carrito
 document.addEventListener('DOMContentLoaded', () => {
     const botonesAgregar = document.querySelectorAll('.btn-agregar');
+    loadProductos()
 
     botonesAgregar.forEach(boton => {
         boton.addEventListener('click', () => {
@@ -13,22 +15,9 @@ document.addEventListener('DOMContentLoaded', () => {
             agregarAlCarrito();
         });
     });
+  })
 
-    const isLoggedIn = sessionStorage.getItem('loggedIn'); 
-    const loginLink = document.getElementById('loginLink');
-    const logoutLink = document.getElementById('logoutLink');
-    const registerlink = document.getElementById('registerLink');
-        
-    if (isLoggedIn === 'true') {
-        loginLink.classList.add('hide-on-login');
-        registerlink.classList.add('hide-on-login');
-        logoutLink.classList.remove('hide-on-login');
-    } else {
-        loginLink.classList.remove('hide-on-login');
-        registerlink.classList.remove('hide-on-login');
-        logoutLink.classList.add('hide-on-login');
-    }
-});
+
 const agregarAlCarrito = async () => {
     const productId = document.getElementById('productId').value
 
@@ -66,3 +55,177 @@ const agregarAlCarrito = async () => {
 
     console.log('@@@ response => ', response)
 }
+
+
+//****FUNCIONES PARA PRODUCTOS******
+
+  
+  //AGREGAR PRODUCTOS - CAMBIO
+  const crearProducto = async () => {
+    const productId = document.getElementById('productId').value
+    let producto
+    if (productId){
+      producto = {
+        idproducto: productId,
+        nombre: document.getElementById('nombre').value,
+        descripcion: document.getElementById('descripcion').value,
+        precio: document.getElementById('precio').value,
+        marca: document.getElementById('marca').value,
+        estado: document.getElementById('estado').value,
+        categoria: document.getElementById('categoria').value,
+       //imagen: document.getElementById('imagen').value,
+        
+      }
+    }else {
+    producto = {
+      nombre: document.getElementById('nombre').value,
+      descripcion: document.getElementById('descripcion').value,
+      precio: document.getElementById('precio').value,
+      marca: document.getElementById('marca').value,
+      estado: document.getElementById('estado').value,
+      categoria: document.getElementById('categoria').value,
+      //imagen: document.getElementById('imagen').value,
+      
+    }
+  }
+    const url = `${apiURL}/usuario/vendedor`
+    const method = productId ? 'PUT' : 'POST'
+  
+    console.log('@@@ ruta y metodo => ', url, method, producto) //sal
+    const resultado = await fetch(url, {
+      method: method,
+      body: JSON.stringify(producto)
+    })
+  
+    //mensajes del sistema
+    const response = await resultado.json()
+    if (response.mensaje === 'Producto Creado') {
+      showAlert('Producto Agregado', 'success')
+      loadProductos()
+      productForm.reset()
+    }else if(response.mensaje === 'Producto Actualizado'){
+      showAlert('ProductoActualizado', 'warning')
+      loadProductos()
+      productForm.reset()
+    }else {
+      showAlert('Error al agregar el producto', 'danger')
+    }
+    document.getElementById('productId').value = ''
+  
+    console.log('@@@ response => ', response)
+  } //FIN DE AGREGAR PRODUCTOS
+  
+  
+  //OBTENER PRODUCTO POR ID - DETALLES
+  const getProducto = async (id) => {
+    try {
+      const send = {
+        id: id
+      }
+      const res = await fetch(apiURL + '/usuario/vendedor', {
+        method: 'POST',
+        body: JSON.stringify(send)
+      })
+      const producto = await res.json()
+      if (producto) {
+        document.getElementById('productId').value = producto.idproducto
+        document.getElementById('nombre').value = producto.nombre
+        document.getElementById('descripcion').value = producto.descripcion
+        document.getElementById('precio').value = producto.precio
+        document.getElementById('marca').value = producto.marca
+        document.getElementById('estado').estado = producto.estado
+        document.getElementById('categoria').categoria = producto.categoria
+        //document.getElementById('imagen').value = producto.imagen
+      }
+      console.log('@@ producto =>', producto)
+      } 
+    catch (error) {
+      console.error('Error: ', error)
+    }
+  }
+  //CARGA DE PRODUCTOS
+  const loadProductos = async () => {
+    try {
+      const res = await fetch(apiURL + '/usuario/vendedor', {
+        method: 'GET',
+        headers: {
+          'Content-Type': 'application/json'
+        }
+      })
+      const productos = await res.json()
+      productTableBody.innerHTML = ''
+      productos.forEach((item) => {
+        const row = document.createElement('tr')
+        row.innerHTML =
+        `
+          <td>${item.idproducto}</td>
+          <td>${item.nombre}</td>
+          <td>${item.descripcion}</td>
+          <td>${item.tipo}</td>
+          <td>${item.precio}</td>
+          <td>${item.imagen}</td>
+          <td>
+            <button class="btn btn-warning btn-sm" data_id="${item.idproducto}">Editar</button>
+            <button class="btn btn-danger btn-sm" data_id="${item.idproducto}">Borrar</button>
+          </td>
+        `
+        productTableBody.appendChild(row)
+      })
+    } catch (error) {
+      console.error('Error:', error);
+    }
+  }
+
+//BORRAR PRODUCTOS
+const borrarProducto = async (id) => {
+    try {
+      const send = {
+        id: id
+      }
+      const res = await fetch(apiURL + '/usuario/vendedor', {
+        method: 'DELETE',
+        body: JSON.stringify(send)
+      })
+      const borrado = await res.json()
+      console.log('@@@ borrado =>', borrado.mensaje)
+      if (borrado && borrado.mensaje && borrado.mensaje === 'Producto Borrado') {
+        showAlert('Producto Borrado', 'danger')
+        loadProductos()
+      }
+      console.log('@@@ res => ', borrado)
+    } catch (error) {
+      console.error('Error: ', error)
+    }
+  }
+  
+  productTableBody.addEventListener('click', (e) => {
+    if (e.target.classList.contains('btn-danger')) {
+      borrarProducto(e.target.getAttribute('data_id'))
+    }
+    if (e.target.classList.contains('btn-warning')) {
+      getProducto(e.target.getAttribute('data_id'))
+    }
+  })
+  
+  btnSubmit.addEventListener('click', (event) => {
+    event.preventDefault()
+    crearProducto()
+  })
+  
+  
+  const showAlert = (mensaje, tipo) => {
+    alertContainer.innerHTML = 
+    `
+      <div class="alert alert-${tipo} alert-dismissable fade show" role="alert">
+        ${mensaje}
+        <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close">
+        </button>
+      </div>
+    `
+    setTimeout(() => {
+      alertContainer.innerHTML = ''
+    }, 3000)
+  }
+  
+  
+
